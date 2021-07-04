bd2008b716da3bc553135a0a327ffe73cc990cfe
Ulukman Amangeldiev <ulukmanatageldiuulu@gmail.com>
2021-07-05 03:06:31 +0600

    Add database

--- iptv/main.py
+++ iptv/main.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI
 
 from .shows.main import router as shows_router
+from .db import create_all, database
 
 app = FastAPI()
 app.include_router(shows_router)
+
+
+@app.on_event("startup")
+async def connect():
+    create_all()
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
